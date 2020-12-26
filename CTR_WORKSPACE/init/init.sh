e24d9cdb136044a050a30bf0d9852175629a90ac
MamoruDS <mamoruds.io@gmail.com>
2020-12-27 04:13:50 +0900

    improved: adding uid/gid preset support

--- CTR_WORKSPACE/init/init.sh
+++ CTR_WORKSPACE/init/init.sh
@@ -14,13 +14,19 @@ if [ -z $TZ ]; then
 fi
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
+if [ -z $UID ]; then
+    UID='1000'
+fi
+if [ -z $GID ]; then
+    GID='1000'
+fi
 if [ -z $USER ]; then
     USER='ctr'
 fi
 if [ -z $PASSWORD ]; then
     PASSWORD='localpasswd'
 fi
-useradd -ms /bin/$SHELL $USER \
+useradd -ms /bin/$SHELL --uid=$UID --gid=$GID $USER \
     && usermod -aG sudo $USER \
     && echo $USER ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USER \
     && echo 'Set disable_coredump false' >> /etc/sudo.conf \
