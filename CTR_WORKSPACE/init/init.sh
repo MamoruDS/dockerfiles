493f26c0bb196e4b263d24004104244d37bddafc
MamoruDS <mamoruds.io@gmail.com>
2023-04-01 21:45:05 +0900

    update: more script polish

--- CTR_WORKSPACE/init/init.sh
+++ CTR_WORKSPACE/init/init.sh
@@ -11,6 +11,7 @@ error() {
 info "- container initialization start -"
 
 # channel can be branch or tag
+REPO_URL='https://raw.githubusercontent.com/MamoruDS/dockerfiles'
 SCRIPT_CHANNEL=${SCRIPT_CHANNEL:-main}
 
 TZ=${TZ:-Etc/UTC}
@@ -56,23 +57,20 @@ info "DEPLOY: dotfiles"
 if [ ! -z $CONDA ]; then
     export DOTFILES_PACKAGES="$DOTFILES_PACKAGES,conda"
 fi
-curl -sfL https://raw.githubusercontent.com/MamoruDS/dockerfiles/$SCRIPT_CHANNEL/scripts/deploy_dotfiles.sh | sh -s -- $_USER
+curl -sfL $REPO_URL/$SCRIPT_CHANNEL/scripts/deploy_dotfiles.sh | sh -s -- $_USER
 
 if [ ! -z $CONDA ]; then
     if [ -z $CONDA_HOME ]; then
         CONDA_HOME="$HOME/miniconda"
     fi
     info "INSTALL: conda in $CONDA_HOME"
-    curl -sL https://raw.githubusercontent.com/MamoruDS/dockerfiles/$SCRIPT_CHANNEL/scripts/install_conda.sh -o /conda_install.sh \
-        && chown $_USER /conda_install.sh \
-        && chmod u+x /conda_install.sh \
-        && sudo -H -u $_USER bash -c "/conda_install.sh $_USER $_SHELL $CONDA_HOME"
+    curl -sfL $REPO_URL/$SCRIPT_CHANNEL/scripts/install_conda.sh | sh -s -- $_USER $_SHELL $CONDA_HOME
 fi
 
 if [ ! -z $CUSTOM_NVIM ]; then
     if ! [ -x "$(command -v nvim)" ]; then
         info "INSTALL: neovim"
-        curl -sL https://raw.githubusercontent.com/MamoruDS/vimrc/main/install_neovim.sh | sh
+        curl -sfL https://raw.githubusercontent.com/MamoruDS/vimrc/main/install_neovim.sh | sh
     fi
 fi
 
@@ -80,8 +78,8 @@ if [ -f "/usr/bin/vncserver" ]; then
     info "START: VNC server"
     VNC=$HOME/.vnc
     mkdir -p $VNC \
-        && curl -sL https://raw.githubusercontent.com/MamoruDS/dockerfiles/$SCRIPT_CHANNEL/CTR_WORKSPACE/init/vncpasswd.init -o $VNC/passwd \
-        && curl -sL https://raw.githubusercontent.com/MamoruDS/dockerfiles/$SCRIPT_CHANNEL/CTR_WORKSPACE/init/xstartup.init -o $VNC/xstartup \
+        && curl -sfL $REPO_URL/$SCRIPT_CHANNEL/CTR_WORKSPACE/init/vncpasswd.init -o $VNC/passwd \
+        && curl -sfL $REPO_URL/$SCRIPT_CHANNEL/CTR_WORKSPACE/init/xstartup.init -o $VNC/xstartup \
         && chown -R $_USER $VNC \
         && chmod 755 $VNC/xstartup
     sudo -u $_USER vncserver
