85c5e4fee81fb01b7ea946d9d7998aabbd433135
MamoruDS <mamoruds.io@gmail.com>
2023-04-02 15:27:31 +0900

    feat: write dotfiles environment variables to zshrc during deployment

--- scripts/deploy_dotfiles.sh
+++ scripts/deploy_dotfiles.sh
@@ -14,13 +14,16 @@ if ! [ -x "$(command -v git)" ]; then
 fi
 
 _CMD="( \
-    echo '[ -f ~/.zshrc.dot ] && . ~/.zshrc.dot' > ~/.zshrc \
-    && cd ~ \
+    cd ~ \
     && export DOTFILES_PACKAGES=$DOTFILES_PACKAGES \
     && export DOTTER_BIN_DIR=$DOTTER_BIN_DIR \
     && export DOTFILES_ROOT=$DOTFILES_ROOT \
     && export DOTFILES_LOCAL=${DOTFILES_LOCAL:-~/.dot.local.toml} \
-    && curl -fsSL https://raw.githubusercontent.com/MamoruDS/dotfiles/main/install.sh | sh
+    && curl -fsSL https://raw.githubusercontent.com/MamoruDS/dotfiles/main/install.sh | sh \
+    && echo '[ -f ~/.zshrc.dot ] && . ~/.zshrc.dot' >> ~/.zshrc \
+    && if [ -n \$DOTTER_BIN_DIR ] && echo 'DOTTER_BIN_DIR=\$DOTTER_BIN_DIR' >> ~/.zshrc \
+    && if [ -n \$DOTFILES_ROOT ] && echo 'DOTFILES_ROOT=\$DOTFILES_ROOT' >> ~/.zshrc \
+    && if [ -n \$DOTFILES_LOCAL ] && echo 'DOTTER_BIN_DIR=\$DOTFILES_LOCAL' >> ~/.zshrc
 )"
 
 target_user="$1"
