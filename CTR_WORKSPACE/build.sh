0982328daf1eaa6b6923e9a30459c53b5c0dedfe
MamoruDS <mamoruds.io@gmail.com>
2023-06-08 15:21:19 +0900

    remove: deprecated configs / scripts

--- CTR_WORKSPACE/build.sh
+++
@@ -1,3 +0,0 @@
-docker build --no-cache \
-             -t mamoruio/workspace:local \
-             -f workspace.dockerfile .
--- CTR_WORKSPACE/scripts/update_theme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl -sL https://raw.githubusercontent.com/MamoruDS/dockerfiles/master/CTR_WORKSPACE/.config/zsh_theme_spaceship.sh \
-    > $HOME/.config/zsh_theme_spaceship.sh
-
-curl -sL https://raw.githubusercontent.com/MamoruDS/dockerfiles/master/CTR_WORKSPACE/.config/zsh_theme_highlight.sh \
-    > $HOME/.config/zsh_theme_highlight.sh
-
-echo 'done'
